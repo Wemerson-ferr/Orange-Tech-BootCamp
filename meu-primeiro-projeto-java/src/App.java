0f91abfd6c65f3809a917c71d3225748d528cbd0
Wemerson-ferr <wemerson-b@hotmail.com>
2023-01-19 15:11:50 -0300

    Criando primeiro codigo do bootcamp

---
+++ meu-primeiro-projeto-java/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+        System.out.println("Meu primeiro projeto java do Orange bootcamp");
+    }
+}

89dc646518c5c7576577a9a43e81e95ebcee0fab
Wemerson-ferr <wemerson-b@hotmail.com>
2023-01-20 15:53:15 -0300

    add aula de doc e comentarios

---
+++ documentacao-comentario-java/src/Calculadora.java
@@ -0,0 +1,31 @@
+/**
+* <h1>Calculadora</h1>
+* A Calculadora realiza operações matemáticas entre números inteiros
+* <p>
+* <b>Note:</b> fonte https://glysns.gitbook.io/java-basico/sintaxe/documentacao
+* 
+*
+* @author  Gleyson Sampaio(professor)
+* @version 1.0
+* @since   01/01/2022
+*/
+public class Calculadora {
+    /**
+   * Este método é utilizado para somar dois números inteiros
+   * @param numeroUm explica o que o pararametro é
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return explica o que será retornado no metodo
+   */
+    public int somar(int numeroUm, int numeroDois) {//Dica: mouse encima do metodo
+        return  numeroUm + numeroDois;
+    }
+}
+// No terminal execute o comando abaixo:
+//javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
+/** 
+ * Estas duas estrelinhas acima !!!!!
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
